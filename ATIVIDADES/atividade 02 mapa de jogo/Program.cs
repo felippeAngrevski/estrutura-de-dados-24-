d543f25ebdb4cab3b01114c28d52d53653f9ba61
Felippe Rabuske Angrevski <134168913+felippeAngrevski@users.noreply.github.com>
2024-11-27 20:24:31 -0300

    Add files via upload

---
+++ ATIVIDADES/atividade 02 mapa de jogo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+Console.OutputEncoding = UTF8Enconding.UTF8;
+
+TerrainEnum[,] map = {
+    {
+        TerranEnum.SAND, TerranEnum.SAND, TerranEnum.SAND,
+    },
+    {
+        TerrainEnum.WATER, TerranEnum.WATER, TerrainEnum.WATER,
+    },
+    {
+        TerrainEnum.WALL, TerrainEnum.WALL, TerrainEnum.WALL,
+    },
+    {
+        TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS,
+    }
+};
+
+for (int row = 0; row < map.GetLenght(0); row++) {
+    for (int col = 0; col < map.GetLenght(1); col++) {
+        Console.ForegroundColor = map[row, colum].GetColor();
+        Console.Write(amp[row, col].GetChar() + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.ForegroundColor = ConsoleColor.Gray;
